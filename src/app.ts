eddf126ce8db88da2a302f4110d87856b6eb326a
Estiven Salvador Hurtado Santos <santosestiven46@gmail.com>
2024-08-24 13:36:29 -0500

    First commit

---
+++ src/app.ts
@@ -0,0 +1,72 @@
+import "dotenv/config"
+import { createBot, createProvider, createFlow, addKeyword, EVENTS } from '@builderbot/bot'
+import { MemoryDB } from '@builderbot/bot'
+import { BaileysProvider } from '@builderbot/provider-baileys'
+import { toAsk, httpInject } from "@builderbot-plugins/openai-assistants"
+import { typing } from "./utils/presence"
+
+/** Puerto en el que se ejecutará el servidor */
+const PORT = process.env.PORT ?? 3008
+/** ID del asistente de OpenAI */
+const ASSISTANT_ID = process.env.ASSISTANT_ID ?? ''
+
+/**
+ * Flujo de bienvenida que maneja las respuestas del asistente de IA
+ * @type {import('@builderbot/bot').Flow<BaileysProvider, MemoryDB>}
+ */
+const welcomeFlow = addKeyword<BaileysProvider, MemoryDB>(EVENTS.WELCOME)
+    .addAction(async (ctx, { flowDynamic, state, provider }) => {
+        await typing(ctx, provider)
+        const response = await toAsk(ASSISTANT_ID, ctx.body, state)
+
+        // Dividir la respuesta en chunks y enviarlos secuencialmente
+        const chunks = response.split(/\n\n+/)
+        for (const chunk of chunks) {
+            const cleanedChunk = chunk.trim().replace(/【.*?】/g, "");
+            await flowDynamic([{ body: cleanedChunk }])
+        }
+    })
+
+/**
+ * Función principal que configura y inicia el bot
+ * @async
+ * @returns {Promise<void>}
+ */
+const main = async () => {
+    /**
+     * Flujo del bot
+     * @type {import('@builderbot/bot').Flow<BaileysProvider, MemoryDB>}
+     */
+    const adapterFlow = createFlow([welcomeFlow])
+    console.log('INICIANDO BOT');
+    console.log(PORT);
+    /**
+     * Proveedor de servicios de mensajería
+     * @type {BaileysProvider}
+     */
+    const adapterProvider = createProvider(BaileysProvider, {
+        groupsIgnore: true,
+        readStatus: false
+    })
+
+    /**
+     * Base de datos en memoria para el bot
+     * @type {MemoryDB}
+     */
+    const adapterDB = new MemoryDB()
+
+    /**
+     * Configuración y creación del bot
+     * @type {import('@builderbot/bot').Bot<BaileysProvider, MemoryDB>}
+     */
+    const { httpServer } = await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    httpInject(adapterProvider.server)
+    httpServer(+PORT)
+}
+
+main()
